cd5b032db809eeeed15c1cee2f9f04de6a3d9a50
Leonardo Moura <ldazevedo.19@gmail.com>
2024-08-10 19:22:08 -0300

    alteracoes

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
 function addContact() {
     const name = document.getElementById('name').value;
     const phone = document.getElementById('phone').value;
-    const opçoes = document.getElementById('opçoes').value;
     const email = document.getElementById('email').value;
 
 
-    if (name , phone , email , opçoes) {
+    if (name , phone , email ) {
         const table = document.getElementById('contactTable').getElementsByTagName('tbody')[0];
         const newRow = table.insertRow();
         
         const cell1 = newRow.insertCell(0);
         const cell2 = newRow.insertCell(1);
         const cell3 = newRow.insertCell(2);
-        const cell4 = newRow.insertCell(3);
+        
         
         cell1.textContent = name;
         cell2.textContent = phone;
-        cell3.textContent = opçoes;
-        cell4.textContent = email;
+        cell3.textContent = email;
         
         document.getElementById('contactForm').reset();
     } else {
